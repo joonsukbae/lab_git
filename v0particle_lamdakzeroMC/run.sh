eace55db0700d5ca47b41ca7425e2009fe0d431a
Hyungjun Lee <leehy@cern.ch>
2023-01-13 16:36:46 +0900

    V0particle adding

---
+++ v0particle_lamdakzeroMC/run.sh
@@ -0,0 +1,8 @@
+o2-analysis-lf-lambdakzeroanalysis-mc  --configuration json://configuration.json -b   |
+o2-analysis-lf-lambdakzerobuilder - --configuration json://configuration.json  -b  |
+o2-analysis-event-selection - --configuration json://configuration.json  -b  |
+o2-analysis-multiplicity-table  --configuration json://configuration.json -b   --aod-file @input_data.txt |
+o2-analysis-pid-tpc  --configuration json://configuration.json  -b  |
+o2-analysis-timestamp  --configuration json://configuration.json  -b |
+o2-analysis-track-propagation - --configuration json://configuration.json  -b |
+o2-analysis-collision-converter - --configuration json://configuration.json  -b  
